9d21eebc496173e3df65d196ff06c20624928ad7
Adithyan S Pillai <101512399+XXOriginalXX@users.noreply.github.com>
2024-11-16 23:13:02 +0530

    script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
         result.innerText = '';
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://adanipredictionapp.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ inputs: [input1, input2] }),
